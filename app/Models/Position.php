26aa05a33376ca555b486a26768b7f79f20adf94
marell4n <azzahrarahmadia0@gmail.com>
2025-10-26 20:21:22 +0700

    nambahin fillable di positions juga kehaous WKWKWKWKWa

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Position extends Model
 {
+    protected $fillable = [
+        'nama_jabatan',
+        'gaji_pokok',
+    ];
+    
     public function employees(): HasMany
     {
         return $this->hasMany(Employee::class, 'jabatan_id');
