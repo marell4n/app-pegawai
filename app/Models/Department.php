2eca1c4d98032057a4e761b6f81892fdfc67a559
marell4n <azzahrarahmadia0@gmail.com>
2025-10-26 20:18:58 +0700

    nambahin fillable di department abis kehapus ternyata

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
+    protected $fillable = [
+        'nama_department',
+    ];
 
     public function employees(): HasMany
     {
