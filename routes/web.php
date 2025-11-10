673bc2e991f75da3dae9d78c698274788d1bccdb
marell4n <azzahrarahmadia0@gmail.com>
2025-11-10 22:43:01 +0700

    Add temporary route to run migrations

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\PositionController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\PerformanceReviewController;
 use App\Http\Controllers\DashboardController;
+//
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -15,3 +17,9 @@ Route::resource('departments', DepartmentController::class);
 Route::resource('positions', PositionController::class);
 Route::resource('attendances', AttendanceController::class);
 Route::resource('performance-reviews', PerformanceReviewController::class);
+//
+Route::get('/migrate-and-seed', function () {
+    // PERINGATAN: HANYA DIGUNAKAN UNTUK PERTAMA KALI DEPLOY
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return 'Migrasi dan Seeding Selesai! HAPUS ROUTE INI SEKARANG!';
+});
